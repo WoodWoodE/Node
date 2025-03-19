0fb68338dd85a9a0fe1a645cb5d7e1dacd868fee
Dong <wrkmandng@gmail.com>
2025-03-19 22:34:26 +0900

    Part5. 로그인, 인증 기능 구축 프로젝트 [ 미니 프로젝트 1 ] - 05. RefreshToken에 대해서 06:13 / 10:22 중간 커밋

--- Node-Express-part1/index.js
+++ Node-Express-part1/index.js
@@ -1,12 +1,51 @@
 const express = require('express');
+const jwt = require("jsonwebtoken");
 const app = express();
+
+const data = [
+    {
+        userId: "Han",
+        content: "Hello, Han"
+    }, {
+        userId: "Kim",
+        content: "No, Thanks Kim"
+    }
+]
 
 app.use(express.json());
 
 app.post('/login', (req, res) =>{
     const userId = req.body.userId;
-    const userPw = req.body.userPw;
+    const user = { userId: userId };
+
+    const token = jwt.sign(user, "secret");
+    res.json({token: token});
 })
+
+app.post('/data', authMiddleware, (req, res) => {
+    console.log(req.user)
+    res.json(data);
+})
+
+
+function authMiddleware(req, res, next) {
+    const authRowToken = req.headers["authorization"];
+    const authToken = authRowToken && authRowToken.split(" ")[1];
+    
+    if (authToken == null) {
+        return res.sendStatue(401);
+    }
+
+    const rq = jwt.verify(authToken, 'secret', (err, decoded) => {
+        if (err) {
+            return res.sendStatue(403);
+        }else{
+            req.user = decoded;
+            next();
+        }
+    })
+    console.log(rq)
+}
 
 app.listen(4000, () => {
     console.log('listening on 4000');
